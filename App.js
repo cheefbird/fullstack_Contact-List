f1e4cb08e9f44e5b4a594097fa54f00c2743f06e
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-28 20:20:48 -0700

    Implement navigation in App.js

--- App.js
+++ App.js
@@ -1,19 +1,7 @@
 import React from "react";
-import { StyleSheet } from "react-native";
 
-import Profile from "./src/screens/Profile";
+import AppNavigator from "./routes";
 
-export default class App extends React.Component {
-  render() {
-    return <Profile />;
-  }
+export default function App() {
+  return <AppNavigator />;
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center"
-//   }
-// });
