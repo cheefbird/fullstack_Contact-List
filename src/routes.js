7b940381b194ce5f5916ef4fcc215a1635bf12a3
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-28 22:44:43 -0700

    Add navigation props for titles and styles.

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,33 @@ import { StackNavigator } from "react-navigation";
 import Contacts from "./screens/Contacts";
 import Profile from "./screens/Profile";
 
+import colors from "./utils/colors";
+
 export default StackNavigator({
   Contacts: {
-    screen: Contacts
+    screen: Contacts,
+    navigationOptions: {
+      title: "Contacts"
+    }
   },
   Profile: {
-    screen: Profile
+    screen: Profile,
+    navigationOptions: ({
+      navigation: {
+        state: { params }
+      }
+    }) => {
+      const {
+        contact: { name }
+      } = params;
+
+      return {
+        title: name.split(" ")[0],
+        headerTintColor: "white",
+        headerStyle: {
+          backgroundColor: colors.blue
+        }
+      };
+    }
   }
 });
