2f05e4c95cbd4c03c4fd33edf0c227be815fa844
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-28 23:49:57 -0700

    Implement TabNavigator.

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,17 @@
-import { StackNavigator } from "react-navigation";
+import { TabNavigator } from "react-navigation";
 
 import Contacts from "./screens/Contacts";
-import Profile from "./screens/Profile";
+import Favorites from "./screens/Favorites";
+import User from "./screens/User";
 
-import colors from "./utils/colors";
-
-export default StackNavigator(
-  {
-    Contacts: {
-      screen: Contacts
-    },
-    Profile: {
-      screen: Profile
-    }
+export default TabNavigator({
+  Contacts: {
+    screen: Contacts
   },
-  {
-    initialRouteName: "Contacts"
+  Favorites: {
+    screen: Favorites
+  },
+  User: {
+    screen: User
   }
-);
+});
