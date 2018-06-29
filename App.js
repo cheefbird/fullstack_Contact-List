ce702f83fa3929c206d3c414c6f1ce836a5b7ae8
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-28 20:14:41 -0700

    Test Profile screen in App.js

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 
-import Contacts from "./src/screens/Contacts";
+import Profile from "./src/screens/Profile";
 
 export default class App extends React.Component {
   render() {
-    return <Contacts />;
+    return <Profile />;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   }
+// });
